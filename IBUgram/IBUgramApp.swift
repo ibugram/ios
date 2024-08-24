b86ae2eb7edb1d1e735d70846ea2440658de4487
SSead <smailagicsead@gmail.com>
2024-08-24 11:00:07 +0200

    Initial commit

---
+++ IBUgram/IBUgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBUgramApp.swift
+//  IBUgram
+//
+//  Created by Sead Smailagic on 24. 8. 2024..
+//
+
+import SwiftUI
+
+@main
+struct IBUgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IBUgram/View/Main/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IBUgram
+//
+//  Created by Sead Smailagic on 24. 8. 2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
